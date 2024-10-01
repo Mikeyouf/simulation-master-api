20445e26a540d4861ed9f2c40b5b7d17f867d1ad
Mikeyouf <mik_anarchy@hotmail.com>
2024-10-01 15:46:03 +0200

    test position fleches v6

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import 'glider-js/glider.min.css'; // Importation du fichier CSS de Glider
 import './style.css'; // Ton fichier CSS personnalisé
 
 document.addEventListener('DOMContentLoaded', function () {
-  // const isMobile = window.innerWidth <= 768;
+  const isMobile = window.innerWidth <= 768;
 
   new Glider(document.querySelector('.glider'), {
     slidesToShow: 1,
@@ -12,18 +12,19 @@ document.addEventListener('DOMContentLoaded', function () {
     loop: true,
     controls: true,
     rewind: true,
+    scrollLock: isMobile,
     arrows: {
       prev: '.glider-prev',
       next: '.glider-next'
     }
   });
 
-  // window.addEventListener('resize', function () {
-  //   // Actualise la détection mobile si la fenêtre est redimensionnée
-  //   const isMobile = window.innerWidth <= 768;
-  //   glider.setOption({
-  //     draggable: isMobile
-  //   });
-  // });
+  window.addEventListener('resize', function () {
+    // Actualise la détection mobile si la fenêtre est redimensionnée
+    const isMobile = window.innerWidth <= 768;
+    glider.setOption({
+      draggable: isMobile
+    });
+  });
 
 });
